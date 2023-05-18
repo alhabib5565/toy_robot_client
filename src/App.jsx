c79610a74d6e20b8a5eca0bf757fe0e59db6b0e8
al-habib <alhabib5565@gmail.com>
2023-05-18 07:29:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className="btn">Button</button>
+<button className="btn btn-primary">Button</button>
+<button className="btn btn-secondary">Button</button>
+<button className="btn btn-accent">Button</button>
+<button className="btn btn-ghost">Button</button>
+<button className="btn btn-link">Button</button>
+    </div>
+  );
+};
+
+export default App;
