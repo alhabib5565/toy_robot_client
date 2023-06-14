3c68e06d4d02277f59ec35cd4730973833711b9d
al-habib <alhabib5565@gmail.com>
2023-06-14 09:12:00 +0600

    react tab not work

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Banner from './Banner';
 import Gallary from '../../provider/Gallary';
+import ShopByCategory from '../../provider/ShopByCategory';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <div className='max-w-7xl mx-auto p-2'>
-            <Gallary></Gallary>
+                <Gallary></Gallary>
+                <ShopByCategory></ShopByCategory>
             </div>
         </div>
     );
